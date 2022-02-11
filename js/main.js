624d91cfbac817a9eac4c4ef0eb9999b2cb07557
marcin1989 <marcinlubanski23@gmail.com>
2022-02-11 09:18:12 +0100

    40-js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+function check(n) {
+    if (n >= 100 && n<= 200) {
+        console.log("Liczba spelnia warunek");
+    }  else if (n > 200) {
+        console.log("Liczba nie spelnia warunku");
+    } else if (n < 100) {
+        console.log("Liczba nie spelnia warunku");
+    }
+
+}
+
+    check(200);
+
+
